c46c39ce60e99acadb421cdd741d115445f0adbe
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2022-04-14 14:03:20 +0900

    feat: 랜덤으로 boolean 값을 생성해주는 enum 생성

--- src/main/java/model/Line.java
+++ src/main/java/model/Line.java
@@ -3,9 +3,10 @@ package main.java.model;
 import java.util.ArrayList;
 
 public class Line {
-    private ArrayList<Boolean> points = new ArrayList<>();
+    private ArrayList<RandomBoolean> points = new ArrayList<>();
 
-    public Line (int countOfPerson) {
+    public Line (int countOfPerson) { // 사람수 - 1 만큼
         // 라인의 좌표 값에 선이 있는지 유무를 판단하는 로직 추가
+
     }
 }
---
+++ src/main/java/model/RandomBoolean.java
@@ -0,0 +1,28 @@
+package main.java.model;
+
+import java.util.Arrays;
+
+public enum RandomBoolean {
+
+    TRUE(0, true),
+    FALSE(1, false);
+
+    private final int idx;
+    private final boolean bool;
+
+    RandomBoolean(int idx, boolean bool){
+        this.idx = idx;
+        this.bool = bool;
+    }
+
+    public RandomBoolean getRandomBoolean(int idx){
+        return Arrays.stream(RandomBoolean.values())
+                .filter(r -> r.idx == idx)
+                .findAny()
+                .orElse(null);
+    }
+
+    public boolean getBoolean(){
+        return this.bool;
+    }
+}
