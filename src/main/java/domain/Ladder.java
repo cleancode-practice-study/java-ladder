58755b52a0a23504c6f8d3ffbb7239f67cdb6762
Egenieee <hhgg0925@naver.com>
2022-04-19 08:12:06 +0900

    주석 제거

--- src/main/java/domain/Ladder.java
+++ src/main/java/domain/Ladder.java
@@ -6,7 +6,6 @@ import java.util.List;
 public class Ladder {
     List<Line> lines;
 
-    // 최대 사다리 높이 만큼 라인(라인의 넓이는 playerCount)을 쌓는다.
     public Ladder (int maxLadderHeight, int playerCount){
         lines = new ArrayList<>();
         for (int i = 0 ; i < maxLadderHeight ; i++) {
