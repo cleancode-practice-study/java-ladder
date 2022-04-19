d15c48dbcee40c2e27268db651f3bfb8537ecc9c
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2022-04-20 00:33:16 +0900

    refactor: GameResult 클래스가 게임 결과만 가지고 있도록 수정

--- src/main/java/model/GameResult.java
+++ src/main/java/model/GameResult.java
@@ -3,13 +3,13 @@ package main.java.model;
 import java.util.Map;
 
 public class GameResult {
-    private final GameResultCreator gameResult;
+    private final Map<String, String> gameResult;
 
-    public GameResult(Players players, Ladder ladder, Prizes prizes) {
-        this.gameResult = new GameResultCreator(players, ladder, prizes);
+    public GameResult(Map<String, String> gameResult) {
+        this.gameResult = gameResult;
     }
 
     public Map<String, String> getGameResult() {
-        return this.gameResult.getGameResult();
+        return this.gameResult;
     }
 }
