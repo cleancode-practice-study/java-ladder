4fa350ff33d22fdbc7bc370e9ebf6a14784143ef
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2022-04-20 00:30:56 +0900

    refactor: OutputView model 호출 리팩토링

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,6 +1,6 @@
 package main.java.view;
 
-import main.java.model.*;
+import main.java.model.Line;
 
 import java.util.List;
 import java.util.Map;
@@ -14,11 +14,17 @@ public class OutputView {
     private static void printLine(List<Boolean> points) {
         for (int i = 0; i < points.size(); i++) {
             if (i != points.size() - 1) {
-                if (points.get(i)) System.out.print("| ----- ");
-                else System.out.print("|       ");
+                if (points.get(i)) {
+                    System.out.print("| ----- ");
+                } else {
+                    System.out.print("|       ");
+                }
             } else {
-                if (points.get(i)) System.out.print("| ----- |");
-                else System.out.print("|       |");
+                if (points.get(i)) {
+                    System.out.print("| ----- |");
+                } else {
+                    System.out.print("|       |");
+                }
             }
         }
     }
@@ -26,19 +32,21 @@ public class OutputView {
     private static void printLadderResults(List<String> prizes) {
         for (String prize : prizes)
             System.out.printf("%-8s", prize);
+
         System.out.println("");
     }
 
-    public static void printLadder(Players players, Ladder ladder, Prizes prizes) {
+    public static void printLadder(List<String> players, List<Line> ladder, List<String> prizes) {
         System.out.println("\n사다리 결과\n");
-        printPlayersNames(players.getPlayers());
+        printPlayersNames(players);
         System.out.println(" ");
-        for (Line line : ladder.getLadder()) {
+
+        for (Line line : ladder) {
             System.out.print("  ");
             printLine(line.getPoints());
             System.out.println("");
         }
-        printLadderResults(prizes.getPrizes());
+        printLadderResults(prizes);
     }
 
     public static void printPlayerNameLengthErrorMessage() {
@@ -49,25 +57,22 @@ public class OutputView {
         System.out.println("두명 이상 입력 해 주세요. \n");
     }
 
-    public static void printResultsCountErrorMessage(Players players) {
-        System.out.println(players.getPlayersCount() + "개의 실행 결과를 입력 해 주세요.");
+    public static void printResultsCountErrorMessage(int playersCount) {
+        System.out.println(playersCount + "개의 실행 결과를 입력 해 주세요.");
     }
 
     public static void printGameResultErrorMessage() {
         System.out.println("해당 플레이어는 존재하지 않습니다. \n다시 입력 해 주세요.");
     }
 
-    public static void printPeopleResult(GameResult gameResult, String name) {
+    public static void printPeopleResult(Map<String, String> gameResult, String name) {
         System.out.println("\n실행 결과");
-        Map<String, String> results = gameResult.getGameResult();
-        System.out.println(results.get(name));
+        System.out.println(gameResult.get(name));
     }
 
-    public static void printAllPeopleResult(GameResult gameResult) {
-        Map<String, String> results = gameResult.getGameResult();
-
+    public static void printAllPeopleResult(Map<String, String> gameResult) {
         System.out.println("\n실행 결과");
-        for (String name : results.keySet())
-            System.out.println(name + " : " + results.get(name));
+        for (String name : gameResult.keySet())
+            System.out.println(name + " : " + gameResult.get(name));
     }
 }
