b7000848e730d725cf6390a9b615498fadc820e4
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2022-04-22 02:18:47 +0900

    test: 실행 결과 리스트를 생성하는 경우

---
+++ src/test/java/PrizesTest.java
@@ -0,0 +1,24 @@
+import model.Prizes;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PrizesTest {
+    @Test
+    void 실행_결과_리스트를_생성하는_경우() {
+        //given
+        String[] names = {"3000", "5000", "꽝", "500"};
+        Prizes prizes = new Prizes(names);
+
+        //when
+        List<String> prize = prizes.getPrizes();
+
+        //then
+        assertThat(prize.get(0)).isEqualTo("3000");
+        assertThat(prize.get(1)).isEqualTo("5000");
+        assertThat(prize.get(2)).isEqualTo("꽝");
+        assertThat(prize.get(3)).isEqualTo("500");
+    }
+}
