524d91aa4c9882d9949f71060d5e112589404978
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2022-04-22 02:11:37 +0900

    test: 플레이어 이름 길이 test 추가

--- src/test/java/ValidatorTest.java
+++ src/test/java/ValidatorTest.java
@@ -1,2 +1,30 @@
+import model.Validator;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class ValidatorTest {
+    @Test
+    void 플레이어_이름의_길이가_기준을_넘은_경우() {
+        //given
+        String[] names = {"ha", "jinhee", "doo"};
+
+        //when
+        boolean result = Validator.isValidPlayerNameLength(names);
+
+        //then
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void 플레이어_이름의_길이가_기준을_넘지않는_경우() {
+        //given
+        String[] names = {"ha", "jin", "dowon"};
+
+        //when
+        boolean result = Validator.isValidPlayerNameLength(names);
+
+        //then
+        assertThat(result).isTrue();
+    }
 }
