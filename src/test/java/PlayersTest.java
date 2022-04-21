af470222e6bfc24b557ce052ca79244199f6ec04
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2022-04-22 02:21:19 +0900

    test: 플레이어 리스트를 생성하는 경우

---
+++ src/test/java/PlayersTest.java
@@ -0,0 +1,26 @@
+import model.Players;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PlayersTest {
+    @Test
+    void 플레이어_리스트를_생성하는_경우() {
+        //given
+        String[] names = {"halim", "jinhe", "dowon"};
+        Players players = new Players(names);
+
+        //when
+        List<String> player = players.getPlayers();
+        int count = players.getPlayerCount();
+
+        //then
+        assertThat(player.get(0)).isEqualTo("halim");
+        assertThat(player.get(1)).isEqualTo("jinhe");
+        assertThat(player.get(2)).isEqualTo("dowon");
+
+        assertThat(count).isEqualTo(3);
+    }
+}
