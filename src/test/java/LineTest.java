f1526788a12253b04e360f3fc6a21824a7df607f
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2022-04-22 13:57:07 +0900

    test: 연속된 TURE가 생성되지 않도록 라인을 생성되는지 확인하는 경우

--- src/test/java/LineTest.java
+++ src/test/java/LineTest.java
@@ -15,4 +15,23 @@ public class LineTest {
         //then
         assertThat(line.getLine().size()).isEqualTo(width);
     }
+
+    @Test
+    void 연속된_TRUE가_존재하지_않도록_라인이_생성되는지_확인하는_경우() {
+        //given
+        int width = 3;
+
+        //when
+        Line line = new Line(width);
+        int trueIdx = 0;
+        for (int i = 0; i < line.getLine().size() - 1; i++) {
+            if (line.getLine().get(i)) {
+                trueIdx = i;
+                break;
+            }
+        }
+
+        //then
+        assertThat(line.getLine().get(trueIdx + 1)).isFalse();
+    }
 }
