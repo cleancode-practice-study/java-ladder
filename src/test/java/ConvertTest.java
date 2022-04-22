954b3c599bab3b02ff1ae73811d4f8dfc59f2292
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2022-04-22 16:12:57 +0900

    test: ConvertTest

---
+++ src/test/java/ConvertTest.java
@@ -0,0 +1,21 @@
+import model.Convert;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+public class ConvertTest {
+    @Test
+    void 쉼표_기준으로_자르는_경우() {
+        //given
+        String name = "halim,jinhee,dowon";
+
+        //when
+        String[] names = Convert.splitNames(name);
+
+        //then
+        assertThat(names[0]).isEqualTo("halim");
+        assertThat(names[1]).isEqualTo("jinhee");
+        assertThat(names[2]).isEqualTo("dowon");
+    }
+
+}
