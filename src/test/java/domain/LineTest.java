acba645265cd8cbfe384759d675c786e81fa9136
Egenieee <hhgg0925@naver.com>
2022-04-25 07:29:40 +0900

    -test: LineTest 작성

---
+++ src/test/java/domain/LineTest.java
@@ -0,0 +1,16 @@
+package domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LineTest {
+    @Test
+    public void 가로_라인_생성() {
+        int playerCount = 3;
+
+        Line line = new Line(playerCount);
+
+        assertThat(line.LineLength()).isEqualTo(playerCount - 1);
+    }
+}
