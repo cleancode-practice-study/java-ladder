63f7ad6e5eca8cc7e99c9625030ae15d876c4671
Egenieee <hhgg0925@naver.com>
2022-04-25 07:50:03 +0900

    -test: RandomLadderCreatorTest 작성

---
+++ src/test/java/domain/RandomLadderCreatorTest.java
@@ -0,0 +1,20 @@
+package domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RandomLadderCreatorTest {
+    @Test
+    public void 랜덤_사다리_생성() {
+        int playerCount = 3;
+        int maxLadderHeight = 3;
+
+        List<Line> lines = RandomLadderCreator.getRandomLadder(playerCount, maxLadderHeight);
+
+        assertThat(lines.size()).isEqualTo(maxLadderHeight);
+        assertThat(lines.get(0).LineLength()).isEqualTo(playerCount - 1);
+    }
+}
