b935fc21fcb2ec1bcc43cf3edb043e2a56011543
Egenieee <hhgg0925@naver.com>
2022-04-29 15:55:59 +0900

    -refactor: RandomLadderCreator 로직 수정

--- src/main/java/domain/RandomLadderCreator.java
+++ src/main/java/domain/RandomLadderCreator.java
@@ -1,16 +1,13 @@
 package domain;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class RandomLadderCreator {
     public static List<Line> getRandomLadder(int playerCount, int maxLadderHeight) {
-        List<Line> lines = new ArrayList<>();
-
-        IntStream.range(0, maxLadderHeight)
-                .forEach(i -> lines.add(new Line(new RandomLineCreator().getLine(playerCount))));
-
-        return lines;
+        return IntStream.range(0, maxLadderHeight)
+                .mapToObj(line -> new Line(new RandomLineCreator().getLine(playerCount)))
+                .collect(Collectors.toList());
     }
 }
