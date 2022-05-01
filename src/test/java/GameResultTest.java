38915467c2c4f4c5db274cb1ccec0efb038aa849
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2022-05-02 05:47:05 +0900

    test: GameResultTest 추가

---
+++ src/test/java/GameResultTest.java
@@ -0,0 +1,29 @@
+import model.GameResult;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class GameResultTest {
+    @Test
+    void 게임_결과를_생성하는_경우(){
+        //given
+        Map<String, String> gameResult = new HashMap<String, String>(){
+            {
+                put("halim", "1000");
+                put("dowon", "2000");
+                put("jinhe", "3000");
+            }
+        };
+
+        //when
+        GameResult result = new GameResult(gameResult);
+
+        //then
+        assertThat(result.getGameResult().get("halim")).isEqualTo("1000");
+        assertThat(result.getGameResult().get("dowon")).isEqualTo("2000");
+        assertThat(result.getGameResult().get("jinhe")).isEqualTo("3000");
+    }
+}
