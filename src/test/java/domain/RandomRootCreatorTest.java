88307a9dccec5bbc208493a25469f9ff4eba71d6
Egenieee <hhgg0925@naver.com>
2022-05-02 17:30:08 +0900

    -test: RandomRootCreatorTest 수정

--- src/test/java/domain/RandomRootCreatorTest.java
+++ src/test/java/domain/RandomRootCreatorTest.java
@@ -7,7 +7,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class RandomRootCreatorTest {
     @Test
     public void 랜덤_사다리_루트_생성() {
-        boolean isRoot = RandomRootCreator.getRandomRoot();
+        RootCreator rootCreator = new RandomRootCreator();
+        boolean isRoot = rootCreator.createRoot();
 
         assertThat(isRoot).isNotNull();
     }
