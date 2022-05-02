c9147cb0fd491348eef6ddc5b32b4ae2187d176c
Egenieee <hhgg0925@naver.com>
2022-05-02 17:27:21 +0900

    -refactor: RandomLineCreator 수정

--- src/main/java/domain/RandomLineCreator.java
+++ src/main/java/domain/RandomLineCreator.java
@@ -6,24 +6,25 @@ import java.util.List;
 public class RandomLineCreator {
     public List<Boolean> getLine (int playerCount) {
         List<Boolean> points = new ArrayList<>();
+        RootCreator rootCreator = new RandomRootCreator();
 
-        addRoot(points, RandomRootCreator.getRandomRoot());
+        addRoot(points, rootCreator, rootCreator.createRoot());
 
         for (int i = 1 ; i < playerCount - 1 ; i++) {
             boolean previousRoot = points.get(i - 1);
-            addRoot(points, previousRoot);
+            addRoot(points, rootCreator, previousRoot);
         }
 
         return points;
     }
 
-    private void addRoot(List<Boolean> points, boolean previousRoot) {
+    private void addRoot(List<Boolean> points, RootCreator rootCreator, boolean previousRoot) {
         if (previousRoot) {
             points.add(false);
         }
 
         if (!previousRoot) {
-            points.add(RandomRootCreator.getRandomRoot());
+            points.add(rootCreator.createRoot());
         }
     }
 }
